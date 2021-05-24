64d10bf36c6db4d5d9d0efd659a44ea09a6a17a2
jared970 <jared@the-dybbuk-box.com>
2021-05-24 22:06:19 +0300

    upload solution basae

---
+++ public_html/include/head.php
@@ -0,0 +1,22 @@
+<head>
+    <meta charset="utf-8" />
+    <title>Crypton Donation</title>
+
+    <meta name="description" content="Donate Crypton to this user to help them with their projects" />
+    <meta name="keywords" content="crypton,utopia,donation,service,p2p,utopia-p2p,utopia-ecosystem" />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Crypton Donation" />
+    <meta property="og:title" content="Crypton Donation" />
+    <meta property="og:description" content="Donate Crypton to this user to help them with their projects" />
+    <meta property="og:url" content="https://crypton.life" />
+    <meta property="og:locale" content="en_EN" />
+    <meta property="og:image" content="img/preview.png" />
+    <meta property="og:image:width" content="800" />
+    <meta property="og:image:height" content="450" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.6.22/dist/css/uikit.min.css" />
+    <link rel="stylesheet" href="css/styles.css?v=<?php echo getenv('version'); ?>" />
+</head>
---
+++ public_html/index.php
@@ -0,0 +1,39 @@
+<?php
+  require_once __DIR__ . '/../vendor/autoload.php';
+  $app = new App\Solution();
+?>
+<html>
+  <?php include_once './include/head.php'; ?>
+  <body>
+    <div class="uk-padding uk-text-lead uk-light">
+      <div class="uk-text-center">
+        <img src="img/logo.svg" style="width: 128px;" />
+        <div class="uk-container">
+          <h2>Create your donation link<br/>in Crypton</h2>
+        </div>
+
+        <div class="uk-width-medium" style="margin: 15px auto;">
+          <form class="uk-form-large uk-padding-small" method="GET" action="CRP">
+            <div class="uk-margin">
+              <input class="uk-input uk-display-block" type="text" placeholder="Crypton address..." />
+              <button class="uk-button uk-button-primary uk-display-block uk-width-1-1 uk-text-center">Create</button>
+            </div>
+          </form>
+        </div>
+
+        <br/>
+        <hr/>
+        <div class="uk-text-center">
+          <div class="uk-width-xlarge" style="margin: auto;">
+            <h1>What is Crypton?</h1>
+            <p class="uk-text-left">Crypton is a truly breakthrough cryptocurrency that guarantees complete privacy protection. It is fully protected from any regularly-related risks, unlike any other cryptocurrency launched by any world-famous or even just public company. No government authority of any country can influence Crypton’s market rate or its fate. As well as there is no way to take control of Crypton or the entire <a href="https://u.is/">Utopia ecosystem</a> and its team that stays and will always stay anonymous.</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <!-- UIkit JS -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.22/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.22/dist/js/uikit-icons.min.js"></script>
+  </body>
+</html>
